b6c9fb5a408403706cdf6fdb027682fe7fc0807d
Sergey Bondarenko <sergibondarenko@gmail.com>
2015-05-04 15:31:59 +0200

    Added multitasking (fork)

---
+++ multitask_server/TCPServer.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>	// for sockaddr_in and inet_ntoa()
+#include <stdlib.h>		// for atoi()
+#include <string.h>		//	for memset
+#include <unistd.h>		//	for clode()
+
+void DieWitError(char *errMSG);						// Error handling function
+void HandleTCPClient(int clntSocket);				// TCP client handling 
+int CreateTCPServerSocket(unsigned short port);	// Create TCP server socket 
+int AcceptTCPConnection(int servSock);				// Accept TCP connection req
