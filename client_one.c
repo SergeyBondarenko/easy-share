68a046979d725080eb6c890379132f03d9193e59
Sergey Bondarenko <sergibondarenko@gmail.com>
2015-06-01 13:04:51 +0200

    Added download function

--- client_one.c
+++ client_one.c
@@ -8,54 +8,120 @@
 #define BUFFSIZE 4096
 // default port 31337
 
-int fileUPLOAD(int socketDESC, char *lfile, char *rfile)
+int parseARGS(char **args, char *line)
 {
-char remoteFILE[BUFFSIZE];
-FILE *fileSEND;
+	int tmp = 0;
+	args[tmp] = strtok(line, ":");
+	while((args[++tmp] = strtok(NULL, ":")) != NULL);
+	return tmp - 1;
+}
 
-// Open local file to copy
-fileSEND = fopen (lfile, "rb");
-if(!fileSEND) {
-    printf("Error opening file\n");
-    close(socketDESC);
-    return 0;
-} else {
-    long fileSIZE;
-    fseek(fileSEND, 0, SEEK_END);                               // Set the file position of the stream to THE END
-    fileSIZE = ftell(fileSEND);                                 // Returns current position (END) of the stream
-    rewind(fileSEND);                                           // Set the file position to the BEGIN
+int fileDOWNLOAD(int socketDESC, char *lfile, char *rfile)
+{
+	char remoteFILE[BUFFSIZE];
+	char *header[BUFFSIZE];
+	char *filename, *filesize;
+	long sent_bytes, recv_bytes, total_bytes;
+	FILE *recvFILE;
+	
+	sprintf(remoteFILE,"DOWNLOAD:%s:%s\r\n", lfile, rfile);
+	
+	total_bytes = 0;
+	while(total_bytes != sizeof(remoteFILE)){
+		if((sent_bytes = send(socketDESC, remoteFILE, sizeof(remoteFILE), 0)) < 0)
+			printf("fileDOWNLOAD() Failed to send file name info!\n");
+		total_bytes += sent_bytes;
+	}
+	
+	total_bytes = 0;
+	while(total_bytes != sizeof(remoteFILE)){
+		if((recv_bytes = recv(socketDESC, remoteFILE, sizeof(remoteFILE), 0)) < 0)
+			printf("fileDOWNLOAD() Failed to receive file size info!\n");
+		total_bytes += recv_bytes;
+	}
+	//printf("%s\n", remoteFILE);
+	
+	remoteFILE[strlen(remoteFILE) - 2] = 0;
+	parseARGS(header, remoteFILE);
+	filename = header[1];
+	filesize = header[2];
+	
+	printf("Filename: %s\n", filename);
+	printf("Filesize: %d B\n", atoi(filesize));
+	
+	recvFILE = fopen(filename, "wb");
+	
+	total_bytes = atoi(filesize);
+	long to_read = atoi(filesize);
+	char *buffer = malloc(atoi(filesize));
+	
+	do{
+		if((recv_bytes = recv(socketDESC, buffer, to_read, 0)) < 0)
+			printf("fileDOWNLOAD() Failed to recv the file!\n");
+		
+	   printf("33[0;0H");
+	   printf( "\33[2J");
+	   printf("Filename: %s\n", filename);
+	   printf("Filesize: %ld B\n", (long)atoi(filesize));
+	
+		total_bytes -= recv_bytes;
+	   printf("Total received: %ld%% (%ld B); Now received: %ld B\n", 100-((total_bytes*100)/(long)atoi(filesize)), total_bytes, recv_bytes);
+	   printf("---\n");
 
-    // Get info of a sending file and send to the server
-    sprintf(remoteFILE,"UPLOAD:%s:%ld\r\n", rfile, fileSIZE);
-    if(send(socketDESC, remoteFILE, sizeof(remoteFILE), 0) < 0)
-        printf("Failed to send file info!\n");
+		fwrite(buffer, 1, recv_bytes, recvFILE);
+	} while(total_bytes != 0);
 
-    long bytes_sent = 0, total_sent = 0, to_send = fileSIZE, bytes_read;
-    long fileSLICE = fileSIZE/10;                               // Set slice to 10% of the file
-    char *buffer = malloc(fileSLICE);                           // Allocate memmory for the slice
-
-    // Read and send the file by slices, 10% each
-    do {
-        bytes_read = fread(buffer, 1, fileSLICE, fileSEND);
-        if((bytes_sent = send(socketDESC, buffer, bytes_read, 0)) < 0)
-            printf("Failed to send the file!\n");
-
-        printf("33[0;0H");
-        printf( "\33[2J");
-        printf("Filename: %s\n", lfile);
-        printf("Filesize: %ld B\n", fileSIZE);
-
-        total_sent += bytes_sent;
-        printf("Total sent: %ld%% (%ld B); Now sent: %ld B\n", (total_sent*100)/fileSIZE, total_sent, bytes_sent);
-        printf("---\n");
-
-        to_send -= bytes_sent;
-    } while(to_send != 0);                                      // Repeat the loop while 0 Bytes are left to send
+	fclose(recvFILE);
 
 }
 
-fclose(fileSEND);                                               // Close the file
-
+int fileUPLOAD(int socketDESC, char *lfile, char *rfile)
+{
+	char remoteFILE[BUFFSIZE];
+	FILE *fileSEND;
+	
+	// Open local file to copy
+	fileSEND = fopen (lfile, "rb");
+	if(!fileSEND) {
+	    printf("Error opening file\n");
+	    return 1;
+	} else {
+	    long fileSIZE;
+	    fseek(fileSEND, 0, SEEK_END);                               // Set the file position of the stream to THE END
+	    fileSIZE = ftell(fileSEND);                                 // Returns current position (END) of the stream
+	    rewind(fileSEND);                                           // Set the file position to the BEGIN
+	
+	    // Get info of a sending file and send to the server
+	    sprintf(remoteFILE,"UPLOAD:%s:%ld\r\n", rfile, fileSIZE);
+	    if(send(socketDESC, remoteFILE, sizeof(remoteFILE), 0) < 0)
+	        printf("Failed to send file info!\n");
+	
+	    long bytes_sent = 0, total_sent = 0, to_send = fileSIZE, bytes_read;
+	    long fileSLICE = fileSIZE/10;                               // Set slice to 10% of the file
+	    char *buffer = malloc(fileSLICE);                           // Allocate memmory for the slice
+	
+	    // Read and send the file by slices, 10% each
+	    do {
+	        bytes_read = fread(buffer, 1, fileSLICE, fileSEND);
+	        if((bytes_sent = send(socketDESC, buffer, bytes_read, 0)) < 0)
+	            printf("Failed to send the file!\n");
+	
+	        printf("33[0;0H");
+	        printf( "\33[2J");
+	        printf("Filename: %s\n", lfile);
+	        printf("Filesize: %ld B\n", fileSIZE);
+	
+	        total_sent += bytes_sent;
+	        printf("Total sent: %ld%% (%ld B); Now sent: %ld B\n", (total_sent*100)/fileSIZE, total_sent, bytes_sent);
+	        printf("---\n");
+	
+	        to_send -= bytes_sent;
+	    } while(to_send != 0);                                      // Repeat the loop while 0 Bytes are left to send
+	
+	}
+	
+	fclose(fileSEND);                                               // Close the file
+	
 }
 
 int fileTRANSFER(char *serverIP, char *PORT, char *CMD, char *lfile, char *rfile){
@@ -85,13 +151,13 @@ int fileTRANSFER(char *serverIP, char *PORT, char *CMD, char *lfile, char *rfile
     }
 
     // Execute commands
-    if(!strcmp(CMD, "upload")){
+    if(!strcmp(CMD, "upload"))
         // Upload file to server
         fileUPLOAD(socketDESC, lfile, rfile);
-    } else if(!strcmp(CMD, "download")){
+    else if(!strcmp(CMD, "download"))
         // Download file from server
-        printf("Download\n");
-    } else
+		  fileDOWNLOAD(socketDESC, lfile, rfile);
+    else
         printf("Available commands: upload, download\n");
 
     // Close the socket
--- server_one.c
+++ server_one.c
@@ -9,53 +9,104 @@
 //#define PORT 31337
 #define BUFFSIZE 4096
 
-int parseARGS(char **args, char *line){
-    int tmp=0;
-    args[tmp] = strtok( line, ":" );                           // Set token to ":"
-    // Fill up "args" array with parts of the "line" string (replacing ":" with NULL)
-    while ( (args[++tmp] = strtok(NULL, ":" ) ) != NULL );
-    return tmp - 1;
+int parseARGS(char **args, char *line)
+{
+	int tmp=0;
+	args[tmp] = strtok( line, ":" );                           // Set token to ":"
+	// Fill up "args" array with parts of the "line" string (replacing ":" with NULL)
+	while ( (args[++tmp] = strtok(NULL, ":" ) ) != NULL );
+	return tmp - 1;
 }
 
-void fileDOWNLOAD(int connectSOCKET, char *infoBUFF)
+int fileDOWNLOAD(int connectSOCKET, char *infoBUFF)
 {
 
+	char *lfilename, *rfilename, *filesize;
+	FILE *sendFILE;                             // File pointer
+	char *header[BUFFSIZE];                     // Array to store file info (filename, filesize)
+	char localFILE[BUFFSIZE];
+	
+	infoBUFF[strlen(infoBUFF) - 2] = 0;         // Mark end of string by eliminating last '/n'
+	parseARGS(header, infoBUFF);                // Parse infoBUFF and copy fields in header[]
+	lfilename = header[1];
+	rfilename = header[2];
+	
+	printf("Filename: local -> %s remote -> %s\n", lfilename, rfilename);
+	
+	sendFILE = fopen(lfilename, "rb");
+	if(!sendFILE){
+		printf("fileDOWNLOAD() Error openning file!\n");
+		return 1;
+	} else {
+		long fileSIZE, sent_bytes, total_bytes, read_bytes;
+		fseek(sendFILE, 0, SEEK_END);
+		fileSIZE = ftell(sendFILE);
+		rewind(sendFILE);
+	
+		sprintf(localFILE, "DOWNLOAD:%s:%ld\r\n", rfilename, fileSIZE);
+		
+		total_bytes = 0;
+		while(total_bytes != sizeof(localFILE)){
+			if((sent_bytes = send(connectSOCKET, localFILE, sizeof(localFILE), 0)) < 0)
+				printf("fileDOWNLOAD() Failed to send file info!\n");
+			total_bytes += sent_bytes;
+		}
+	
+		total_bytes = fileSIZE;
+		long fileSLICE = fileSIZE/10; 
+		char *buffer = malloc(fileSLICE);
+		do{
+			read_bytes = fread(buffer, 1, fileSLICE, sendFILE);
+			if((sent_bytes = send(connectSOCKET, buffer, read_bytes, 0)) < 0)
+				printf("fileDOWNLOAD() failed to send file!\n");
+
+			total_bytes -= sent_bytes;
+	      printf("To send: %ld\n", total_bytes);
+	      printf("Data sent: %ld\n", sent_bytes);
+	      printf("---\n");
+			//printf("Total sent: %ld%% (%ld B); Now sent: %ld B\n", (total_bytes*100)/fileSIZE, total_bytes, sent_bytes);
+		} while(total_bytes != 0);
+		
+	}
+		
+	fclose(sendFILE);
+	
 }
 
 void fileUPLOAD(int connectSOCKET, char *infoBUFF)
 {
-char *filename, *filesize;
-FILE *recvFILE;                             // File pointer
-char *header[BUFFSIZE];                     // Array to store file info (filename, filesize)
-
-infoBUFF[strlen(infoBUFF) - 2] = 0;         // Mark end of string by eliminating last '/n'
-parseARGS(header, infoBUFF);                // Parse infoBUFF and copy fields in header[]
-filename = header[1];
-filesize = header[2];
-
-printf("Filename: %s\n", filename);
-printf("Filesize: %d B\n", atoi(filesize));
-
-recvFILE = fopen(filename, "wb");
-
-int bytes_read = 0, to_read = atoi(filesize);
-char *buffer = malloc(atoi(filesize));
-
-// Receive remote file and write it into local file
-do {
-    if((bytes_read = recv(connectSOCKET, buffer, to_read, 0)) < 0)
-        printf("Failed to recv the file!\n");
-
-    printf("To read: %d\n", to_read);
-    printf("Data read: %d\n", bytes_read);
-    printf("---\n");
-    to_read -= bytes_read;
-
-    fwrite(buffer, 1, bytes_read, recvFILE);
-} while(to_read != 0);                      // Loop while there is nothing to receive
-
-fclose(recvFILE);
-
+	char *filename, *filesize;
+	FILE *recvFILE;                             // File pointer
+	char *header[BUFFSIZE];                     // Array to store file info (filename, filesize)
+	
+	infoBUFF[strlen(infoBUFF) - 2] = 0;         // Mark end of string by eliminating last '/n'
+	parseARGS(header, infoBUFF);                // Parse infoBUFF and copy fields in header[]
+	filename = header[1];
+	filesize = header[2];
+	
+	printf("Filename: %s\n", filename);
+	printf("Filesize: %d B\n", atoi(filesize));
+	
+	recvFILE = fopen(filename, "wb");
+	
+	int bytes_read = 0, to_read = atoi(filesize);
+	char *buffer = malloc(atoi(filesize));
+	
+	// Receive remote file and write it into local file
+	do {
+	    if((bytes_read = recv(connectSOCKET, buffer, to_read, 0)) < 0)
+	        printf("Failed to recv the file!\n");
+	
+	    printf("To read: %d\n", to_read);
+	    printf("Data read: %d\n", bytes_read);
+	    printf("---\n");
+	    to_read -= bytes_read;
+	
+	    fwrite(buffer, 1, bytes_read, recvFILE);
+	} while(to_read != 0);                      // Loop while there is nothing to receive
+	
+	fclose(recvFILE);
+	
 }
 
 void *ThreadRoutine(void *ptr){
@@ -86,6 +137,11 @@ int main(int argc, char* argv[])
     socklen_t clientADDRESSLENGTH[512];                             // Array of client addr length
     struct sockaddr_in clientADDRESS[512], serverADDRESS;       // Array of client addr and server addr
     pthread_t threads[512];                                             // Array of thread IDs, max 512
+
+	 if(argc != 2){
+		printf("Syntax: %s <Port>\n", argv[0]);
+		return 1;
+	 }
 
     // Create socket for incomming connection
     listenSOCKET = socket(AF_INET, SOCK_STREAM, 0);
@@ -137,4 +193,6 @@ int main(int argc, char* argv[])
 
     // Close socket
     close(listenSOCKET);
+
+	return 0;
 }
