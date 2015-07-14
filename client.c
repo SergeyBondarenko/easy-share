97bfc1ae50fc7c84f7cd568de6bbad6d235c6e0f
Sergey Bondarenko <sergibondarenko@gmail.com>
2015-07-14 15:30:45 +0000

    Download func added!

--- client.c
+++ client.c
@@ -5,131 +5,253 @@
 #include <string.h>
 #include <unistd.h>
 
-#define RCVBUFSIZE 4096 // Size of receive buffer
+#define BUFFSIZE 4096 // Size of receive buffer
 
+int parseARGS(char **args, char *line);
 void DieWithError(char *errorMessage); // Error handling function
+void UploadFile(int sock, char *lfile, char *rfile);
+void DownloadFile(int sock, char *lfile, char *rfile);
+void SysCmd(int sock, char *myCommand);
 
 int main(int argc, char *argv[])
 {
-	int i, percent_sent, sock;								// Socket descriptor
-	struct sockaddr_in servAddr;	// Echo server address
-	unsigned short servPort;		// Echo server port
-	char *servIP;							// Server IP addr
-	char *myCommand, *commandOpt, *lfile, *rfile;
-	char *echoString;						//	String to send to echo server
-	char buffer[RCVBUFSIZE];		// Buffer for echo string
-	unsigned int echoStringLen, commandStringLen;		// Length of string to echo
-	int bytesRcvd, totalBytesRcvd;		// Bytes (B) read in single recv() and total B read
-	long bytes_recvd, bytes_sent, bytes_read, bytes_total, bytes_left, file_size;
-	FILE *aFile;
+   int i, percent_sent, sock;                      // Socket descriptor
+   struct sockaddr_in servAddr;  // Echo server address
+   unsigned short servPort;      // Echo server port
+   char *servIP;                    // Server IP addr
+   char *myCommand, *commandOpt, *lfile, *rfile;
 
-	if((argc != 6)){
-		fprintf(stderr, "Usage: %s <Server IP> [<Echo Port>] <Command> <File> <New File>\n", argv[0]);
-		exit(1);
-	}
+   // Check cmd args
+   if(argc == 6 || argc == 4){
+      servIP = argv[1];                // Server IP addr
+      servPort = atoi(argv[2]);
+      myCommand = argv[3];
+      if(argc == 6){
+         lfile = argv[4];
+         rfile = argv[5];
+      }
+   } else {
+      fprintf(stderr, "Usage: %s <Server IP> [<Echo Port>] <Command> <File> <New File>\n", argv[0]);
+      exit(1);
+   }
 
-	servIP = argv[1];						// Server IP addr
-	servPort = atoi(argv[2]);
-	myCommand = argv[3];
-	lfile = argv[4];
-	rfile = argv[5];
+   // Create a reliable, stream socket using TCP
+   if((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+      DieWithError("socket() failed!");
 
-	// Create a reliable, stream socket using TCP
-	if((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-		DieWithError("socket() failed!");
-	
-	// Construct the server address structure
-	memset(&servAddr, 0, sizeof(servAddr)); 	// Zero out structure
-	servAddr.sin_family = AF_INET;						// Internet addr family
-	servAddr.sin_addr.s_addr = inet_addr(servIP);	// Server IP addr. inet_addr transforms IP addr to bin format
-	servAddr.sin_port = htons(servPort);			// Server port. htons make sure bytes are stored in big endian 
+   // Construct the server address structure
+   memset(&servAddr, 0, sizeof(servAddr));   // Zero out structure
+   servAddr.sin_family = AF_INET;                  // Internet addr family
+   servAddr.sin_addr.s_addr = inet_addr(servIP);   // Server IP addr. inet_addr transforms IP addr to bin format
+   servAddr.sin_port = htons(servPort);         // Server port. htons make sure bytes are stored in big endian 
 
-	// Establish connection to echo server
-	if(connect(sock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
-		DieWithError("connect() failed!");
+   // Establish connection to echo server
+   if(connect(sock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
+      DieWithError("connect() failed!");
 
-	// Open file for read in binary mode
-	aFile = fopen(lfile, "rb");
-	if(!aFile)
-		DieWithError("fopen() failed!\n");
-	
-	fseek(aFile, 0, SEEK_END);
-	file_size = ftell(aFile);
-	rewind(aFile);			
+   // Select a cmd command
+   if(!strcmp(myCommand, "upload") && argc == 6)
+      UploadFile(sock, lfile, rfile);
+   else if(!strcmp(myCommand, "download") && argc == 6)
+      DownloadFile(sock, lfile, rfile);
+   else if(!strcmp(myCommand, "dir") && argc == 4)
+      SysCmd(sock, myCommand);
+   else{
+      printf("Available commands: dir, upload, download.\nExamples:\n-----\n");
+      printf("./client <Server IP> <Server PORT> dir\n");
+      printf("./client <Server IP> <Server PORT> upload localfile.txt remotefile.txt\n");
+      printf("./client <Server IP> <Server PORT> download remotefile.txt localfile.txt\n-----\n");
+   }
 
-	memset(buffer, 5, sizeof(buffer));
-	sprintf(buffer, "UPLOAD:%s:%ld\r\n", rfile, file_size);
-
-	int all_bytes_sent = 0;
-	while(all_bytes_sent != sizeof(buffer)){
-		bytes_sent = send(sock, (buffer + all_bytes_sent), (sizeof(buffer) - all_bytes_sent), 0);
-
-		if(bytes_sent < 0)
-			DieWithError("send() UPLOAD msg failed!\n");
-
-		all_bytes_sent += bytes_sent;
-	}
-
-	//char *file_buffer = (char *)malloc(chunk_size * sizeof(char));	
-	//if(NULL == file_buffer){
-	//	fprintf(stderr, "malloc failed\n");
-	//	return(-1);
-	//}
-
-	int chunk_size = 256;
-	char file_buffer[chunk_size];
-	bytes_total = bytes_left = file_size;
-
-	printf("\n-----\n");
-	while(1){
-		char file_buffer[chunk_size];
-		memset(file_buffer, 0, sizeof(file_buffer));
-
-		if((bytes_read = fread(file_buffer, sizeof(char), chunk_size, aFile)) < 0)
-			DieWithError("fread() failed!\n");
-
-		if(bytes_read > 0){
-			if((bytes_sent = send(sock, file_buffer, bytes_read, 0)) < 0)
-				DieWithError("send() after fread() failed!");
-			
-			bytes_left -= bytes_sent;
-			percent_sent = ((bytes_total-bytes_left)*100)/bytes_total;
-			printf("Sent %d%% (%ld B), remaining = %ld B\n", percent_sent, bytes_sent, bytes_left);
-		}
-
-		if(bytes_read < chunk_size){
-			if(feof(aFile))
-				printf("End of file.\n");
-			if(ferror(aFile))
-				printf("Error reading!\n");
-			break;
-		}
-	}
-
-	//memset(file_buffer, 0, sizeof(file_buffer));
-	//while((bytes_read = fread(file_buffer, sizeof(char), sizeof(file_buffer), aFile)) > 0){
-	//	if((bytes_sent = send(sock, file_buffer, bytes_read, 0)) < 0)
-	//		DieWithError("send() after fread() failed!");
-
-	//	bytes_left -= bytes_sent;
-	//	percent_sent = ((bytes_total-bytes_left)*100)/bytes_total;
-
-	//	printf("Sent %d%% (%ld B), remaining = %ld B\n", percent_sent, bytes_sent, bytes_left);
-	//	memset(file_buffer, '0', sizeof(file_buffer));
-	//}	
-
-	fclose(aFile);
-	//free(file_buffer);
-
-	printf("\n");
-
-	close(sock);
-	exit(0);
+   // Close the socket
+   close(sock);
+   exit(0);
 
 }
 
 void DieWithError(char *errorMsg)
 {
-	perror(errorMsg);
-	exit(1);
+   perror(errorMsg);
+   exit(1);
 }
+
+void UploadFile(int sock, char *lfile, char *rfile)
+{  
+   int percent_sent, chunk_size = 256;
+   long all_bytes_sent, bytes_sent, bytes_read, bytes_left, file_size;
+   char buffer[BUFFSIZE];      // Buffer for echo string
+   FILE *aFile;
+
+   // Open file for read in binary mode
+   aFile = fopen(lfile, "rb");
+   if(!aFile)
+      DieWithError("fopen() failed!\n");
+
+   // Set pos indicator to the END of file 
+   // and return curr pos      
+   fseek(aFile, 0, SEEK_END);
+   file_size = ftell(aFile);
+   rewind(aFile);
+
+   bytes_left = file_size;
+
+   // Init buffer with 0s
+   // then fill it with file data
+   memset(buffer, 0, sizeof(buffer));
+   sprintf(buffer, "UPLOAD:%s:%ld\r\n", rfile, file_size);   
+
+   all_bytes_sent = 0;
+   while(all_bytes_sent != sizeof(buffer)){
+      bytes_sent = send(sock, (buffer + all_bytes_sent), (sizeof(buffer) - all_bytes_sent), 0);
+
+      if(bytes_sent < 0)
+         DieWithError("send() UPLOAD msg failed!\n");
+
+      all_bytes_sent += bytes_sent;
+   }
+
+   //// Allocate dyn memmory to store file before sending 
+   //char *file_buffer = (char *)malloc(chunk_size * sizeof(char));  
+   //if(NULL == file_buffer){
+   // fprintf(stderr, "malloc failed\n");
+   // return(-1);
+   //}
+
+   printf("\n-----\n");
+
+   // Loop until all Bytes of the file will be send
+   while(1){
+      char file_buffer[chunk_size];
+      memset(file_buffer, 0, sizeof(file_buffer));
+
+      // Read file to file_buffer
+      if((bytes_read = fread(file_buffer, sizeof(char), chunk_size, aFile)) < 0)
+         DieWithError("fread() failed!\n");
+
+      // Send file over socket
+      if(bytes_read > 0){
+         if((bytes_sent = send(sock, file_buffer, bytes_read, 0)) < 0)
+            DieWithError("send() after fread() failed!");
+
+         // Calc percentage and display status
+         bytes_left -= bytes_sent;
+         percent_sent = ((file_size - bytes_left) * 100) / file_size;
+         printf("Sent %d%% (%ld B), remaining = %ld B\n", percent_sent, bytes_sent, bytes_left);
+      }
+
+      // Check the end of file
+      // and if it is the end - break loop
+      if(bytes_read < chunk_size){
+         if(feof(aFile))
+            printf("End of file.\n");
+         if(ferror(aFile))
+            printf("Error reading!\n");
+         break;
+      }
+   }
+
+   // Close the file
+   fclose(aFile);
+   //free(file_buffer);
+
+   printf("\n");
+}
+
+void DownloadFile(int sock, char *lfile, char *rfile)
+{
+   //printf("Download func is under construction!\n");
+   //exit(1);
+   long all_bytes_sent, bytes_sent, bytes_read, bytes_left, file_size;
+   long all_bytes_recvd, bytes_recvd, bytes_written;
+   char buffer[BUFFSIZE];      // Buffer for echo string
+	char *file_name, *header[BUFFSIZE];
+   FILE *aFile;
+
+	// Create and send status message for DOWNLOAD
+	memset(buffer, 0, sizeof(buffer));
+	sprintf(buffer, "DOWNLOAD:%s:%d\r\n", lfile, 1);
+
+	printf("%s\n", buffer);
+   
+	all_bytes_sent = 0;
+   while(all_bytes_sent != sizeof(buffer)){
+      bytes_sent = send(sock, (buffer + all_bytes_sent), (sizeof(buffer) - all_bytes_sent), 0);
+
+      if(bytes_sent < 0)
+         DieWithError("send() DOWNLOAD msg failed!\n");
+
+      all_bytes_sent += bytes_sent;
+   }
+
+	// Receive stat message for DOWNLOAD
+	memset(buffer, 0, sizeof(buffer));
+	all_bytes_recvd = 0;
+   while(all_bytes_recvd != sizeof(buffer)){
+      bytes_recvd = recv(sock, (buffer + all_bytes_recvd), (sizeof(buffer) - all_bytes_recvd), 0);
+
+      if(bytes_recvd < 0)
+         DieWithError("recv() STAT msg failed!\n");
+      if(bytes_recvd == 0)
+         printf("Received STAT msg!\n");
+
+      all_bytes_recvd += bytes_recvd;
+      printf("STAT: Received %ld B, remaining data = %ld B\n", bytes_recvd, (sizeof(buffer) - all_bytes_recvd));
+   }
+
+	printf("%s\n", buffer);	
+
+	parseARGS(header, buffer);	
+	file_name = header[1];
+	file_size = atoi(header[2]);
+
+	aFile = fopen(rfile, "wb");
+	if(aFile == NULL)
+		DieWithError("failed to open the file!\n");
+	
+	memset(buffer, 0, sizeof(buffer));
+	all_bytes_recvd = 0;
+	while(all_bytes_recvd != file_size){
+		bytes_recvd = recv(sock, buffer, sizeof(buffer), 0);
+		if(bytes_recvd < 0)
+			DieWithError("failed to receive the file!\n");		
+		
+		if((bytes_written = fwrite(buffer, sizeof(char), bytes_recvd, aFile)) < 0)
+			DieWithError("failed to write into the file!\n");
+
+		all_bytes_recvd += bytes_recvd;
+		printf("Received %ld B, remaining data = %ld B\n", bytes_recvd, (file_size - all_bytes_recvd));
+	}	
+
+	fclose(aFile);
+}
+
+
+void SysCmd(int sock, char *myCommand)
+{
+   //printf("System commands func is under construction!\n");  
+   //exit(1);
+	long bytes_recvd, bytes_sent;
+	char buffer[BUFFSIZE];
+
+	// Create and send status message for DOWNLOAD
+	sprintf(buffer, "EXEC:%s:%d\r\n", myCommand, 1);
+	if((bytes_sent = send(sock, buffer, sizeof(buffer), 0)) < 0)
+		DieWithError("send() failed!\n");
+
+	bzero(buffer, sizeof(buffer));
+
+	if((bytes_recvd = recv(sock, buffer, sizeof(buffer), 0)) < 0)
+		DieWithError("recv() failed!\n");
+
+	printf("Files in . dir:\n%s\n", buffer);
+	bzero(buffer, sizeof(buffer));
+}
+
+int parseARGS(char **args, char *line)
+{
+   int tmp = 0;
+   args[tmp] = strtok(line, ":");
+   while ((args[++tmp] = strtok(NULL, ":")) != NULL);
+   return tmp - 1;
+}
+
--- lib/HandleTCPClient.c
+++ lib/HandleTCPClient.c
@@ -4,96 +4,181 @@
 
 #define BUFFSIZE 4096
 
-int parseARGS(char **args, char *line){
+int parseARGS(char **args, char *line);
+void UploadFile(int clntSock, char *buffer);
+void DownloadFile(int clntSock, char *buffer);
+void SysCmd(int clntSock, char *buffer);
+
+void HandleTCPClient(int clntSock)
+{
+	char buffer[BUFFSIZE];
+	long bytes_recvd, all_bytes_recvd;
+
+	memset(buffer, 0, sizeof(buffer));
+
+	all_bytes_recvd = 0;
+	while(all_bytes_recvd != sizeof(buffer)){
+		bytes_recvd = recv(clntSock, (buffer + all_bytes_recvd), (sizeof(buffer) - all_bytes_recvd), 0);
+
+		if(bytes_recvd < 0)
+			DieWithError("recv() STAT msg failed!\n");
+		if(bytes_recvd == 0)
+			printf("Received STAT msg!\n");
+
+		all_bytes_recvd += bytes_recvd;
+		printf("STAT: Received %ld B, remaining data = %ld B\n", bytes_recvd, (sizeof(buffer) - all_bytes_recvd));
+	}
+
+	if(!strncmp(buffer, "UPLOAD", 6))
+		UploadFile(clntSock, buffer);
+	else if(!strncmp(buffer, "DOWNLOAD", 8))
+		DownloadFile(clntSock, buffer);
+	else if(!strncmp(buffer, "EXEC", 4))
+		SysCmd(clntSock, buffer);
+	
+}
+
+int parseARGS(char **args, char *line)
+{
    int tmp=0;
    args[tmp] = strtok( line, ":" );
    while ( (args[++tmp] = strtok(NULL, ":" ) ) != NULL );
    return tmp - 1;
 }
 
-void HandleTCPClient(int clntSock)
+void UploadFile(int clntSock, char *buffer)
 {
-	char buffer[BUFFSIZE], *file_name, *header[BUFFSIZE];
-	long bytes_recvd, bytes_wrote, file_size, bytes_total;
+	char *file_name, *header[BUFFSIZE];
+	long bytes_recvd, bytes_written, file_size, all_bytes_recvd;
 	FILE *aFile;
 
-	memset(buffer, 0, sizeof(buffer));
+	parseARGS(header, buffer);
+	file_name = header[1];
+	file_size = atoi(header[2]);
 
-	// Receive message from client
-	//if((bytes_recvd = recv(clntSock, buffer, sizeof(buffer), 0)) < 0)
-	//	DieWithError("recv() failed\n");
+	aFile = fopen(file_name, "wb");
+	if(aFile == NULL)
+		DieWithError("failed to open the file!\n");
+	
+	//memset(buffer, 0, sizeof(buffer));
+	memset(buffer, 0, BUFFSIZE);
 
-	int all_bytes_recvd = 0;
-	while(all_bytes_recvd != sizeof(buffer)){
-		bytes_recvd = recv(clntSock, (buffer + all_bytes_recvd), (sizeof(buffer) - all_bytes_recvd), 0);
+	all_bytes_recvd = 0;
 
-		if(bytes_recvd < 0)
-			DieWithError("recv() UPLOAD msg failed!\n");
-		if(bytes_recvd == 0)
-			printf("Received UPLOAD status msg!\n");
-
+	//while((bytes_recvd = recv(clntSock, buffer, sizeof(buffer), 0)) > 0){
+	while((bytes_recvd = recv(clntSock, buffer, BUFFSIZE, 0)) > 0){
 		all_bytes_recvd += bytes_recvd;
-		printf("STAT: Received %ld B, remaining data = %ld B\n", bytes_recvd, (sizeof(buffer) - all_bytes_recvd));
+		printf("Received %ld B, remaining data = %ld B\n", bytes_recvd, (file_size - all_bytes_recvd));
+
+		if((bytes_written = fwrite(buffer, sizeof(char), bytes_recvd, aFile)) < 0)
+			DieWithError("fwrite() failed!\n");
 	}
 
-	if(!strncmp(buffer, "UPLOAD", 6)){
-		parseARGS(header, buffer);
-		file_name = header[1];
-		file_size = atoi(header[2]);
+	fclose(aFile);	
+}
 
-		//char *file_buffer = malloc(file_size);
-		//char file_buffer[file_size];
-		//bzero(file_buffer, file_size);
-		//int chunk_size = 256;
-		//char file_buffer[chunk_size];
-		//memset(file_buffer, 0, sizeof(file_buffer)); 
-		//bytes_total = file_size;
+void DownloadFile(int clntSock, char *sbuffer)
+{
+	//int chunk_size = 256;
+	int percent_sent;
+	long bytes_sent, bytes_read, file_size, all_bytes_sent, bytes_left;
+	char buffer[BUFFSIZE], *file_name, *header[BUFFSIZE];
+	FILE *aFile;
 
-		aFile = fopen(file_name, "wb");
-		if(aFile == NULL)
-			DieWithError("failed to open the file!\n");
+	printf("%s\n", sbuffer);
+	parseARGS(header, sbuffer);
+	file_name = header[1];
+
+	aFile = fopen(file_name, "rb");
+	if(aFile == NULL)
+		DieWithError("failed to open the file!\n");
+
+	fseek(aFile, 0, SEEK_END);
+	file_size = ftell(aFile);
+	rewind(aFile);
+
+	bytes_left = file_size;
 	
-		//char file_buffer[file_size];
-		memset(buffer, 0, sizeof(buffer));
+	memset(buffer, 0, BUFFSIZE);
+	sprintf(buffer, "DOWNLOAD:%s:%ld\r\n", file_name, file_size);
+	printf("%s\n", buffer);
 
-		all_bytes_recvd = 0;
-		//while(all_bytes_recvd != sizeof(file_buffer)){
-		//	bytes_recvd = recv(clntSock, (file_buffer + all_bytes_recvd), (sizeof(file_buffer) - all_bytes_recvd), 0);
+	all_bytes_sent = 0;
+	while(all_bytes_sent != BUFFSIZE){
+		bytes_sent = send(clntSock, (buffer + all_bytes_sent), (BUFFSIZE - all_bytes_sent), 0);
+		if(bytes_sent < 0)
+			DieWithError("send STAT msg failed!\n");		
 
-		//	if(bytes_recvd < 0)
-		//		DieWithError("recv() UPLOAD msg failed!\n");
-		//	if(bytes_recvd == 0)
-		//		printf("Received UPLOAD status msg!\n");
+		all_bytes_sent += bytes_sent;
+		printf("STAT: Sent %ld B, remaining data = %ld B\n", bytes_sent, (BUFFSIZE - all_bytes_sent));
+	}
 
-		//	all_bytes_recvd += bytes_recvd;
-		//	printf("UPLOAD: Received %ld B, remaining data = %ld B\n", bytes_recvd, (sizeof(file_buffer) - all_bytes_recvd));
+	while(1){
+		memset(buffer, 0, BUFFSIZE);
 
-		//	if((bytes_wrote = fwrite((file_buffer + all_bytes_recvd), sizeof(char), bytes_recvd, aFile)) < 0)
-		//		DieWithError("fwrite() failed!\n");
-		//}
+		if((bytes_read = fread(buffer, sizeof(char), BUFFSIZE, aFile)) < 0)
+			DieWithError("failed to read the file!\n");	
 
-		while((bytes_recvd = recv(clntSock, buffer, sizeof(buffer), 0)) > 0){
-			all_bytes_recvd += bytes_recvd;
-			printf("Received %ld B, remaining data = %ld B\n", bytes_recvd, (file_size - all_bytes_recvd));
+		if(bytes_read > 0){
+			if((bytes_sent = send(clntSock, buffer, bytes_read, 0)) < 0)
+				DieWithError("fialed to send the file!\n");
 
-			if((bytes_wrote = fwrite(buffer, sizeof(char), bytes_recvd, aFile)) < 0)
-				DieWithError("fwrite() failed!\n");
+			bytes_left -= bytes_sent;
+			percent_sent = ((file_size - bytes_left) * 100) / file_size;
+			printf("Sent %d%% (%ld B), remaining = %ld B\n", percent_sent, bytes_sent, bytes_left);
 		}
 
-		//while((bytes_recvd = recv(clntSock, file_buffer, sizeof(file_buffer), 0)) > 0){
-		//	bytes_total -= bytes_recvd;
-		//	printf("Received %ld B from file's data, remaining data = %ld\n", bytes_recvd, bytes_total);
-
-		//	if((bytes_wrote = fwrite(file_buffer, sizeof(char), bytes_recvd, aFile)) < 0)
-		//		DieWithError("fwrite() failed!\n");
-		//}
-
-		fclose(aFile);	
-		//free(file_buffer);
-	
-	} else {
-		printf("Not UPLOAD\n");
+		if(bytes_read < BUFFSIZE){
+			if(feof(aFile))
+				printf("End of file.\n");
+			if(ferror(aFile))
+				printf("Error reading!\n");
+			break;
+		}
 	}
 	
-	
+	fclose(aFile);	
+
+}
+
+void SysCmd(int clntSock, char *buffer)
+{
+//	char *cmd_name, *file_name, *header[BUFFSIZE];
+//	char local_buffer[BUFFSIZE];
+//	long bytes_sent, bytes_recvd;;
+//	DIR *dir;
+//	struct dirent *dp;
+//
+//	printf("%s\n", buffer);
+//	parseARGS(header, buffer);
+//	cmd_name = header[1];
+//
+//	if(!strcmp(cmd_name, "dir")){
+//		dir = opendir (".");
+//		if(!dir)
+//			DieWithError("opendir() failed!\n");
+//		else{
+//			//char *cmd_buffer = malloc(BUFFSIZE);
+//			//bzero(cmd_buffer, BUFFSIZE);
+//
+//			while ((dp=readdir(dir)) != NULL) {
+//				if ( !strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..") ){
+//        		    // do nothing (straight logic)
+//        		} else {
+//        		    file_name = dp->d_name; // use it
+//					strcat(local_buffer, file_name);
+//					strcat(local_buffer, ":");
+//					//sprintf(cmd_buffer, "%s", dp->d_name);
+//					//printf("%s\n", cmd_buffer);
+//        		    //printf("file_name: \"%s\"\n",file_name);
+//        		}	
+//			}
+//
+//			printf("%s\n", local_buffer);
+//			if((bytes_sent = send(clntSock, local_buffer, sizeof(local_buffer), 0)) < 0)	
+//				DieWithError("send() failed!\n");
+//			bzero(local_buffer, sizeof(local_buffer));
+//		}
+//		closedir(dir);
+//	}
 }
